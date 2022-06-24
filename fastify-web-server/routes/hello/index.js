def59ed4c905f916e797b28e40724441376dbc43
zacbe <zacbegonzalez@gmail.com>
2022-06-24 14:19:45 +0200

    first commit 🚀

---
+++ fastify-web-server/routes/hello/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const hello = `<html>
+  <head>
+    <style>
+     body { background: #333; margin: 1.25rem }
+     h1 { color: #EEE; font-family: sans-serif }
+    </style>
+  </head>
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>`;
+
+module.exports = async function (fastify, opts) {
+  fastify.get("/", async function (request, reply) {
+    // return 'this is an example'
+    reply.type("text/html");
+    return hello;
+  });
+};
